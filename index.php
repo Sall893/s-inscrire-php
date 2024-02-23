8ebe36a0e3c4483a5c9e915323f9ecd2a4a0c553
Souleymane sall <sallyuri893@gmail.com>
2024-02-23 15:34:26 +0100

    first commit

--- index.php
+++ index.php
@@ -2,95 +2,32 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=<, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="index.css">
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
     <title>Document</title>
 </head>
 <body>
     <div class="wrapper">
-    <form method="POST" action="traitement.php">
-        <h2>Veuillez vous inscrire</h2>
-        <div class="input-box">
-        <label for="nom">Votre nom</label>
-        <input type="text" id="nom" name="nom" placeholder="Entrez votre nom..."required>
-        </div>
-        <div class="input-box">
-        <label for="prenom">Votre prenom</label>
-        <input type="text" id="prenom" name="prenom" placeholder="Entrez votre prenom..."required>
-        </div>
-        <div class="input-box">
-        <label for="pseudo">Votre mot de passe</label>
-        <input type="text" id="pseudo" name="pseudo" placeholder="Entrez votre pseudo..."required>
-        </div>
-        <div class="input-box">
-        <label for="e-mail">Votre e-mail</label>
-        <input type="text" id="email" name="email" placeholder="Entrez votre email..."required>
-        </div>
-        <div class="input-box">
-        <label for="mot de passe">Votre mot de passe</label>
-        <input type="password" id="pass" name="pass" placeholder="Entrez votre mot de pass..."required>
-        </div>
-        <input type="submit" class="btn"value="M'inscrire" name "ok">
-    </form>
+        <form action="">
+            <h1>Bienvenue sur Quizz IAM</h1>
+            <div class="input-box">
+                <input type="text" placeholder="username"required>
+                <i class='bx bxs-user'></i>
+            </div>
+            <div class="input-box">
+                <input type="password" placeholder="Password"required>
+                <i class='bx bxs-lock-alt'></i>
+            </div>
+           
+            <button type="submitt" class="btn">Se connecter</button>
+            <div class="register-link">
+                <p>Vous n'avez pas de compte ? </p>
+                 <p><a href="inscription.php">S'inscrire</a></p>
+            </div>
+        </form>
     </div>
-    <style>
-        input{
-        margin-bottom:10px;
-        }
-        body{
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            margin-top:200px;
-            border-radius:10px;
-            background:#2c3e50;
 
-        }
-        .wrapper{
-                width: 420px;
-                background: #0082e6;
-                color: #fff;
-                border-radius: 10px;
-                padding: 30px 40px;
-       }
-        .wrapper .input-box{
-                position: relative;
-                width: 100%;
-                height: 50px;
-                margin: 30px 0; 
-       }
-       .input-box input{
-                 width: 100%;
-                height: 100%;
-                background: transparent;
-                border: none;
-                outline: none;
-                border: 2px solid rgba(255, 255, 255, .2);
-              border-radius: 40px;
-        }
-        .input-box input::placeholder{
-                color: #fff;
-  
-  }
-        .input-box i{
-                position: absolute;
-                right: 20px;
-                top: 50%;
-                transform: translate(-50%);
-                font-size: 20px;
-  }
-  .wrapper .btn{
-    width: 100%;
-    height: 45px;
-    background: #fff;
-    border: none;
-    outline: none;
-    border-radius: 40px;
-    box-shadow: 0 0 10px rgba(0, 0, 0, .1);
-    cursor: pointer;
-    font-size: 16px;
-    color: #333;
-    font-weight: 600;
-  }
-    </style>
+
 </body>
 </html>
